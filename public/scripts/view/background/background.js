262a763f35d88f9c1427ae9ff5d751a46a803d29
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2026-02-07 00:02:44 -0300

    clouds added

--- public/scripts/view/background/background.js
+++ public/scripts/view/background/background.js
@@ -1,8 +1,12 @@
 import { Level } from "../../level.js"
 import { hexToRGB, rgbToHex, shadeColor } from "../../utils.js"
+import { Camera } from "../camera.js"
 import { Screen, StaticScreen } from "../screen.js"
+import { Cloud } from "./clouds.js"
 
 export class Background {
+    static clouds = []
+    static wind = {dx : 1}
     static size = {w:785,h:515}
     static timeShade = [
         "#99d9ea",
@@ -14,6 +18,18 @@ export class Background {
         "#FF7F27",
         "#00A2E8",
     ]
+    static addCloud() {
+        let y = Math.random()*Camera.size.h
+        this.clouds.push(new Cloud(0,y,this.wind.dx))
+    }
+    static removeCloud(c) {
+        this.clouds = this.clouds.filter(cl => cl!==c)
+    }
+    static updateClouds() {
+        this.clouds.forEach(c => {
+            c.update()
+        })
+    }
     static getDayTime() {
         return Level.gameTime%Level.day
     }
@@ -24,6 +40,9 @@ export class Background {
         return Math.floor((this.getDayTime()/1000)*24) + "h" + Math.floor((Math.floor((this.getDayTime()/1000)*24*60))%60)
     }
     static draw() {
+        if (this.clouds.length<=5) {
+            this.addCloud()
+        }
         let dayTime = this.getDayTime()
         //console.log(dayTime)
 
@@ -37,6 +56,7 @@ export class Background {
             hexToRGB(this.timeShade[thisShade]),
             hexToRGB(this.timeShade[nextShade]),this.getDayShade()%1))
         StaticScreen.drawRect(0,0,this.size.w,this.size.h,rgbToHex(rgb))
+        this.updateClouds()
         StaticScreen.write(this.getGameTime(),"white",30,700,30)
     }
 }
---
+++ public/scripts/view/background/clouds.js
@@ -0,0 +1,31 @@
+import { Velocity } from "../../entities.js"
+import { checkCollision } from "../../utils.js"
+import { Camera } from "../camera.js"
+import { StaticScreen } from "../screen.js"
+import { Background } from "./background.js"
+
+export class Cloud {
+    static cloudColorList = ["white","gray"]
+    constructor(x,y,dy) {
+        this.x =x
+        this.y =y
+        this.distanceFromView = Math.random()*5
+        this.w = 16*this.distanceFromView
+        this.h = 16
+        this.dx = dy*(1/this.distanceFromView)
+        this.dy = 0
+        this.color = "white"
+    }
+    update() {
+        Velocity.apply(this)
+
+        if (!checkCollision({...Camera.size,x:0,y:0},this)) {
+            Background.removeCloud(this)
+        }
+
+        this.draw()
+    }
+    draw() {
+        StaticScreen.drawRect(this.x,this.y,this.w,this.h,this.color)
+    }
+}
